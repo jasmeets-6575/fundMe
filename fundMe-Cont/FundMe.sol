4ee6c5049da6f8e6dd60f90a096f82cdb64675b3
jasmeets-6575 <jasmeetsonu990@gmail.com>
2023-11-13 15:53:55 +0530

    modifier

--- fundMe-Cont/FundMe.sol
+++ fundMe-Cont/FundMe.sol
@@ -1,5 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConvertor.sol";
+
+error NotOwner();
 
 contract FundMe{
 
@@ -19,7 +24,16 @@ contract FundMe{
         funders.push(msg.sender);
     }
 
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
     
+    modifier onlyOwner {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
 
     function withdraw() public {}
 }
